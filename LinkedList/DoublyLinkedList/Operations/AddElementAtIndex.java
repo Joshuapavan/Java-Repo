fb72359de9b739ff2662717fcdd7846cbf3866c7
Joshua Pavan <joshuapavan35@gmail.com>
2024-02-23 15:46:45 +0530

    adds doubly linked list - insertion of element

---
+++ LinkedList/DoublyLinkedList/Operations/AddElementAtIndex.java
@@ -0,0 +1,45 @@
+package LinkedList.DoublyLinkedList.Operations;
+
+import LinkedList.DoublyLinkedList.Node;
+
+import java.util.Scanner;
+
+public class AddElementAtIndex {
+    public static void main(String[] args) {
+
+        Node head = Node.init();
+
+        System.out.println("Linked list before adding a new node");
+        Traversal.traverseFromStartToEnd(head);
+
+        System.out.println("Enter the value of the node : ");
+        Node newNode = new Node(new Scanner(System.in).nextInt());
+
+        System.out.println("Enter the index, where the element has to be inserted : ");
+        int index = new Scanner(System.in).nextInt();
+
+        addElementAtIndex(head, newNode, index);
+    }
+
+    private static void addElementAtIndex(Node head, Node newNode, int index) {
+
+        if(index == 0){
+            head.previousNode = newNode;
+            newNode.previousNode = null;
+            newNode.nextNode = head;
+            Traversal.traverseFromStartToEnd(newNode);
+            return;
+        }
+        Node currentNode = head;
+        for(int i = 0 ; i < index; i++){
+            currentNode = currentNode.nextNode;
+        }
+
+        newNode.nextNode = currentNode.nextNode;
+        newNode.previousNode = currentNode;
+        currentNode.nextNode = newNode;
+
+
+        Traversal.traverseFromStartToEnd(head);
+    }
+}
