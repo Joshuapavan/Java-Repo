5b71a1077571b95eb2fc2fc1ad42582612d2cfcd
Joshua Pavan <joshuapavan35@gmail.com>
2024-02-28 12:47:49 +0530

    adds structure for binary trees.

---
+++ Trees/BinaryTree.java
@@ -0,0 +1,13 @@
+package Trees;
+
+public class BinaryTree {
+    int value;
+    BinaryTree left;
+    BinaryTree right;
+
+    public BinaryTree(int value){
+        this.value = value;
+        left = null;
+        right = null;
+    }
+}
