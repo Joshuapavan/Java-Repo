ca4460e4d6d5c158f0eb8102ebe87becf9946c77
Joshua Pavan <joshuapavan35@gmail.com>
2024-03-05 15:54:54 +0530

    covers abstract classes and interfaces, multiple inheritance

---
+++ OOPS/AbstractAndInterfaces/AbstractClass.java
@@ -0,0 +1,15 @@
+package OOPS.AbstractAndInterfaces;
+
+/**
+    An abstract class is a class defined with the abstract keyword.
+    it can have abstract methods as well as non-abstract methods.
+ */
+public abstract class AbstractClass {
+
+    public abstract void implementThisAbstractMethod();
+
+    public void nonAbstractMethod(){
+        System.out.println("Hello world");
+    }
+
+}
